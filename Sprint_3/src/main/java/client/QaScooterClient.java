ad89deee3a36221857f5c5d68f5b72faa8f60483
Alexey Dyatlov <alexey.dyatlov@yandex.ru>
2022-04-23 01:48:17 +0300

    update models and client

--- Sprint_3/src/main/java/client/QaScooterClient.java
+++ Sprint_3/src/main/java/client/QaScooterClient.java
@@ -7,7 +7,22 @@ public class QaScooterClient extends QaScooterBaseClient{
 
     private final String baseUrl = "http://qa-scooter.praktikum-services.ru/";
 
-    public Response postLogin(CourierLogin login) {
-        return postRequest(baseUrl + "/api/v1/courier/login", login);
+    public CourierLogin postLogin(CourierLogin login) {
+        return postRequest(baseUrl + "/api/v1/courier/login", login).body()
+                .as(CourierLogin.class);
     }
+
+    public Response createCourier(CourierLogin courierLogin) {
+        return postRequest(baseUrl + "/api/v1/courier", courierLogin);
+    }
+
+    public Response deleteCourier(String id) {
+        return deleteRequest(baseUrl + "/" + id);
+    }
+
+    public CourierLogin getCourierOrders(String id) {
+        return getRequest(baseUrl + "/api/v1/courier/" + id + "/ordersCount")
+                .body().as(CourierLogin.class);
+    }
+
 }
